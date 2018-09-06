9a03a8fcff3c9ebcb943b8ae2826e41e1f8071f6
HiroIshida <spitfire.docomo@gmail.com>
2018-09-06 15:01:09 +0900

    3rd and 4th order Tensor to MxArray

--- include/mexplus/workaround.h
+++ include/mexplus/workaround.h
@@ -36,6 +36,43 @@ mxArray* MxArray::from(const std::vector<Eigen::MatrixXd>& data){
     }
     return numeric_array.release();
 }
+// 3rd order tensor [v, v, ,v , v, v]; [v, v, ,v, ..]; ....;
+template<>
+mxArray* MxArray::from(const std::vector<std::vector<Eigen::VectorXd>>& data){
+    //MxArray numeric_array<double>(2,2);
+    int N_ptcl = data.size();//ptcl
+    int N_model = data[0].size();//model
+    int N_dim = data[0][0].rows();
+    MxArray numeric_array(MxArray::Numeric<double>(N_model*N_dim, N_ptcl));
+    for(int i=0; i<N_ptcl; i++){
+        for(int j=0; j<N_model; j++){
+            for(int k=0; k<N_dim; k++){
+                numeric_array.set(j*N_dim+k, i, data[i][j](k));
+            }
+        }
+    }
+    return numeric_array.release();
+}
+
+// 4th order tensor
+template<>
+mxArray* MxArray::from(const std::vector<std::vector<Eigen::MatrixXd>>& data){
+    int N_ptcl = data.size();//ptcl
+    int N_model = data[0].size();//model
+    int N_rows = data[0][0].rows();
+    int N_cols = data[0][0].cols();
+    MxArray numeric_array(MxArray::Numeric<double>(N_model*N_rows, N_ptcl*N_cols));
+    for(int i=0; i<N_ptcl; i++){
+        for(int j=0; j<N_model; j++){
+            for(int k=0; k<N_rows; k++){
+                for(int l=0; l<N_cols; l++){
+                    numeric_array.set(j*N_rows+k, i*N_cols+l, data[i][j](k, l));
+                }
+            }
+        }
+    }
+    return numeric_array.release();
+}
 
 }
 
